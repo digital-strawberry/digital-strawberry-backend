85b0d993fe972001c708dce0c6f6c7078a442edf
GeorgiySurkov <georgiy.surkov@gmail.com>
2021-11-20 20:13:37 +0300

    chore: create media dir if it doesn't exist

--- app/paths.py
+++ app/paths.py
@@ -2,4 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+# create ./media if doesn't exist
+if not os.path.exists(MEDIA_DIR):
+    os.makedirs(MEDIA_DIR)
 MODELS_DIR = os.path.join(BASE_DIR, 'models')

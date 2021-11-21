2f2abeec473ae4404956413a3bd773143038c865
GeorgiySurkov <georgiy.surkov@gmail.com>
2021-11-21 12:54:47 +0300

    final version

--- app/worker/models_services/diagnose_service.py
+++ app/worker/models_services/diagnose_service.py
@@ -7,7 +7,7 @@ from cv2 import cv2
 from torchvision import models, transforms
 import torchvision.transforms.functional as F
 import torch.nn as nn
-from typing import Dict
+from typing import Dict, List, Set
 
 from app import paths
 
@@ -110,20 +110,20 @@ class DiagnoseService:
                     yield cut
 
     def diagnose(self, file_path: str) -> Dict:
-        # TODO: process case when self.leaf_squares has len = 0
         total_distribution = np.zeros(9)
         square_counter = 0
         for square in self.leaf_squares(file_path):
             picture = self.prepare_square(square)
             total_distribution += self.get_health_distribution(picture)
             square_counter += 1
-        total_distribution /= square_counter
+        if square_counter!=0:
+            total_distribution /= square_counter
 
         health_rate = 1 - max(total_distribution)
         illness_list = []
 
         for i in range(9):
-            if total_distribution[i] > 0.35:
+            if total_distribution[i] > 0.25:
                 illness_list.append(self.label_encoder[i])
 
         return {
@@ -132,11 +132,55 @@ class DiagnoseService:
             'recommendations': self.get_recommendations(illness_list)
         }
 
+    def get_description(self, mark):
+        if mark == 'chem':
+            return None
+        elif '-' in mark:
+            measure, action = mark.split('-')
+            return "{act} {mes}".format(act='Увеличить' if action == 'hi' else 'уменьшить',
+                                        mes=self.measure_dict[measure])
+        else:
+            return "Бороться с {}".format('насекомыми' if mark == 'insects' else 'сорняками')
+
+    def get_recommendations(self, illness_list: List[str]) -> List[Dict[str, str]]:
+        conclusion = {}
+        for disease in illness_list:
+            recommend_dict = self.create_recommendation(disease)
+            for key in recommend_dict:
+                if key not in conclusion:
+                    conclusion[key] = set([])
+                conclusion[key] |= recommend_dict[key]
+        final_keys = [key for key in conclusion if len(conclusion[key]) == 1 or key == 'chem']
+        report = []
+        for key in final_keys:
+            flag = key + '-' + list(conclusion[key])[0] if type(
+                list(conclusion[key])[0]) == str and key != 'chem' else key
+            description = self.get_description(flag)
+            if description is None: description = "Применить {}".format(', '.join(list(conclusion[flag])))
+            report.append({
+                'type': flag,
+                'description': description
+            })
+        return report
+
+    def create_recommendation(self, disease_name: str) -> Dict[str, Set[str]]:
+        guid_keys = self.cure_cookbook[disease_name]
+        recommendation = {}
+        for guid_key in guid_keys:
+            if guid_key == 'chem':
+                recommendation[guid_key] = self.alchemy[disease_name]
+            elif '-' not in guid_key:
+                recommendation[guid_key] = {True}
+            else:
+                key, val = guid_key.split('-')
+                recommendation[key] = {val}
+        return recommendation
+
     def is_healthy(self, photo: np.array) -> bool:
         return bool(self.healthcheck(photo).argmax())
 
     def get_health_distribution(self, photo) -> np.array:
-        return nn.functional.softmax(self.disease_classifier(photo)[0].detach().unsqueeze(0), dim=-1)[0].numpy()
+        return nn.functional.softmax(nn.ReLU()(self.disease_classifier(photo)[0].detach().unsqueeze(0)), dim=-1)[0].numpy()
 
     # load healthcheck and disease_classifier models lazily for graphic memory saving
     @property
